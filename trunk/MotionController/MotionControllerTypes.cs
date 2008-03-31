ad3de6c51e3898e494e8f6df8d98f4b67a61269d
JohannesLiem <JohannesLiem@8d4823a7-1447-0410-9572-df3bbcc21079>
2008-03-31 15:36:26 +0000

    Includes Michael's MotionController, tried to tweak manifest to make IR work but can't test it until we get to TSRB

--- trunk/MotionController/MotionControllerTypes.cs
+++ trunk/MotionController/MotionControllerTypes.cs
@@ -32,23 +32,226 @@ namespace Robotics.CoroBot.MotionController
         /// </summary>
         public const String Identifier = "http://schemas.tempuri.org/2008/02/motioncontroller.html";
     }
+
+    [DataContract]
+    public enum DrivingStates { Stopped, MovingForward, MovingBackward, CalibratingDrive, CalibratingTurn, TurningLeft, TurningRight }
     
     /// <summary>
     /// The MotionController State
     /// </summary>
-    [DataContract()]
+    [DataContract]
     public class MotionControllerState
     {
+        private double _distanceCalibration;
+        private double _turningCalibration;
+        private DrivingStates _drivingState;
+        private double _encoderCountdown;
+        private double _encoderCalibration;
+        private double _power;
+
+        [DataMember]
+        public double DistanceCalibration { get { return _distanceCalibration; } set { _distanceCalibration = value; } }
+
+        [DataMember]
+        public double TurningCalibration { get { return _turningCalibration; } set { _turningCalibration = value; } }
+
+        [DataMember]
+        public DrivingStates DrivingState { get { return _drivingState; } set { _drivingState = value; } }
+
+        [DataMember]
+        public double EncoderCountdown { get { return _encoderCountdown; } set { _encoderCountdown = value; } }
+
+        [DataMember]
+        public double EncoderCalibration { get { return _encoderCalibration; } set { _encoderCalibration = value; } }
+
+        [DataMember]
+        public double Power { get { return _power; } set { _power = value; } }
     }
     
     /// <summary>
     /// MotionController Main Operations Port
     /// </summary>
     [ServicePort()]
-    public class MotionControllerOperations : PortSet<DsspDefaultLookup, DsspDefaultDrop, Get>
+    public class MotionControllerOperations : PortSet<
+        DsspDefaultLookup,
+        DsspDefaultDrop,
+        Get,
+        Drive,
+        Turn,
+        BeginCalibrateDrive,
+        BeginCalibrateTurn,
+        SetDriveCalibration,
+        SetTurnCalibration,
+        Stop>
     {
     }
-    
+
+    public class Drive : Update<DriveRequest, PortSet<DefaultUpdateResponseType, Fault>>
+    {
+        public Drive() { }
+
+        public Drive(DriveRequest body)
+            : base(body)
+        {
+
+        }
+    }
+
+    public class Turn : Update<TurnRequest, PortSet<DefaultUpdateResponseType, Fault>>
+    {
+        public Turn() { }
+
+        public Turn(TurnRequest body)
+            : base(body)
+        {
+
+        }
+    }
+
+    public class BeginCalibrateDrive : Update<BeginCalibrateDriveRequest, PortSet<DefaultUpdateResponseType, Fault>>
+    {
+        public BeginCalibrateDrive() { }
+
+        public BeginCalibrateDrive(BeginCalibrateDriveRequest body)
+            : base(body)
+        {
+
+        }
+    }
+
+    public class BeginCalibrateTurn : Update<BeginCalibrateTurnRequest, PortSet<DefaultUpdateResponseType, Fault>>
+    {
+        public BeginCalibrateTurn() { }
+
+        public BeginCalibrateTurn(BeginCalibrateTurnRequest body)
+            : base(body)
+        {
+
+        }
+    }
+
+    public class SetDriveCalibration : Update<SetDriveCalibrationRequest, PortSet<DefaultUpdateResponseType, Fault>>
+    {
+        public SetDriveCalibration() { }
+
+        public SetDriveCalibration(SetDriveCalibrationRequest body)
+            : base(body)
+        {
+
+        }
+    }
+
+    public class SetTurnCalibration : Update<SetTurnCalibrationRequest, PortSet<DefaultUpdateResponseType, Fault>>
+    {
+        public SetTurnCalibration() { }
+
+        public SetTurnCalibration(SetTurnCalibrationRequest body)
+            : base(body)
+        {
+
+        }
+    }
+
+    public class Stop : Update<StopRequest, PortSet<DefaultUpdateResponseType, Fault>>
+    {
+        public Stop() : base(new StopRequest())
+        {
+
+        }
+    }
+
+    [DataContract]
+    public class DriveRequest
+    {
+        private double _distance;
+        public double Distance { get { return _distance; } set { _distance = value; } }
+
+        private double _power;
+        public double Power { get { return _power; } set { _power = value; } }
+
+        public DriveRequest() { }
+
+        public DriveRequest(double distance)
+        {
+            Distance = distance;
+        }
+
+        public DriveRequest(double distance, double power)
+        {
+            Distance = distance;
+            Power = power;
+        }
+    }
+
+    [DataContract]
+    public class TurnRequest
+    {
+        private double _radians;
+        public double Radians { get { return _radians; } set { _radians = value; } }
+
+        private double _power;
+        public double Power { get { return _power; } set { _power = value; } }
+
+        public TurnRequest() { }
+
+        public TurnRequest(double radians)
+        {
+            Radians = radians;
+        }
+
+        public TurnRequest(double radians, double power)
+        {
+            Radians = radians;
+            Power = power;
+        }
+    }
+
+    [DataContract]
+    public class BeginCalibrateDriveRequest
+    {
+        public BeginCalibrateDriveRequest() { }
+    }
+
+    [DataContract]
+    public class BeginCalibrateTurnRequest
+    {
+        public BeginCalibrateTurnRequest() { }
+    }
+
+    [DataContract]
+    public class SetDriveCalibrationRequest
+    {
+        private double _distance;
+        public double Distance { get { return _distance; } set { _distance = value; } }
+
+        public SetDriveCalibrationRequest() { }
+
+        public SetDriveCalibrationRequest(double distance)
+        {
+            Distance = distance;
+        }
+    }
+
+    [DataContract]
+    public class SetTurnCalibrationRequest
+    {
+        private double _radians;
+        public double Radians { get { return _radians; } set { _radians = value; } }
+
+        public SetTurnCalibrationRequest() { }
+
+        public SetTurnCalibrationRequest(double radians)
+        {
+            Radians = radians;
+        }
+    }
+
+    [DataContract]
+    public class StopRequest
+    {
+        public StopRequest() { }
+    }
+
     /// <summary>
     /// MotionController Get Operation
     /// </summary>
